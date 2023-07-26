1a0eaefbf3f862ec13aeca4473130b7e575c9d3e
cathy <mcathy2410@gmail.com>
2023-07-26 06:54:32 -0700

    4 commit

--- _printf.c
+++ _printf.c
@@ -1,71 +1,67 @@
 #include "main.h"
 
-/****************** FUNCTIONS ******************/
-/* ... (Implement the functions for different conversion specifiers as required) ... */
+void print_buffer(char buffer[], int *buff_ind);
 
-/* Function to print a character */
-int print_char(va_list types, char buffer[], int flags, int width, int precision, int size)
-{
-    // ... (Implement the function to print a character) ...
-}
-
-/* Function to print a string */
-int print_string(va_list types, char buffer[], int flags, int width, int precision, int size)
-{
-    // ... (Implement the function to print a string) ...
-}
-
-/* Function to print a percent sign */
-int print_percent(va_list types, char buffer[], int flags, int width, int precision, int size)
-{
-    // ... (Implement the function to print a percent sign) ...
-}
-
-/* Function to print an integer (decimal) */
-int print_int(va_list types, char buffer[], int flags, int width, int precision, int size)
-{
-    // ... (Implement the function to print an integer) ...
-}
-
-/* Function to print an unsigned integer */
-int print_unsigned(va_list types, char buffer[], int flags, int width, int precision, int size)
-{
-    // ... (Implement the function to print an unsigned integer) ...
-}
-
-/* Function to print an octal number */
-int print_octal(va_list types, char buffer[], int flags, int width, int precision, int size)
-{
-    // ... (Implement the function to print an octal number) ...
-}
-
-/* Function to print a hexadecimal number */
-int print_hexadecimal(va_list types, char buffer[], int flags, int width, int precision, int size)
-{
-    // ... (Implement the function to print a hexadecimal number) ...
-}
-
-/* Function to print a memory address */
-int print_pointer(va_list types, char buffer[], int flags, int width, int precision, int size)
-{
-    // ... (Implement the function to print a memory address) ...
-}
-
-/* Function to print a string in reverse */
-int print_reverse(va_list types, char buffer[], int flags, int width, int precision, int size)
-{
-    // ... (Implement the function to print a string in reverse) ...
-}
-
-/* Function to print a string in ROT13 encoding */
-int print_rot13string(va_list types, char buffer[], int flags, int width, int precision, int size)
-{
-    // ... (Implement the function to print a string in ROT13 encoding) ...
-}
-
-/****************** _PRINTF ******************/
+/**
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
+ */
 int _printf(const char *format, ...)
 {
-    // ... (Keep the rest of the _printf function as it is, including the va_list handling and buffer printing) ...
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
+	}
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
 
